4a86475c63648e52622e084ffbbad9a203b175a8
Lachlan Kuhr <lachlankuhr@gmail.com>
2018-04-28 21:49:26 +1000

    Further fixed merge conflicts.

--- src/test/delivery/RefrigeratedTruckTest.java
+++ src/test/delivery/RefrigeratedTruckTest.java
@@ -1,6 +1,5 @@
 package delivery;
 
-<<<<<<< HEAD
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -12,8 +11,4 @@ public class RefrigeratedTruckTest {
 		fail("Not yet implemented");
 	}
 
-=======
-public class RefrigeratedTruckTest {
-
->>>>>>> 5e1c156d75721b548da4714496833fc611a3c4d0
 }
--- src/test/stock/ItemTest.java
+++ src/test/stock/ItemTest.java
@@ -1,6 +1,5 @@
 package stock;
 
-<<<<<<< HEAD
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -12,8 +11,4 @@ public class ItemTest {
 		fail("Not yet implemented");
 	}
 
-=======
-public class ItemTest {
-
->>>>>>> 5e1c156d75721b548da4714496833fc611a3c4d0
 }
