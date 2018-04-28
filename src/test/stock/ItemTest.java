0499f9adafeb3bf0de5e62c36c22f821a2af4e78
Atrey Gajjar <gajjar_atrey98@hotmail.com>
2018-04-28 22:09:51 +1000

    Initialise @Before for itemTest

--- src/test/stock/ItemTest.java
+++ src/test/stock/ItemTest.java
@@ -2,9 +2,15 @@ package stock;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ItemTest {
+	
+	@Before
+	public void initialise() {
+		Item item = new Item();
+	}
 
 	@Test
 	public void test() {
