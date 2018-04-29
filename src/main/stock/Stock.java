2b3c35d5f9cfc4847752ac99b4501522323dab34
Atrey Gajjar <gajjar_atrey98@hotmail.com>
2018-04-29 14:43:41 +1000

    Making Stock and extension of HashMap and some associated tests

--- src/main/stock/Stock.java
+++ src/main/stock/Stock.java
@@ -1,24 +1,14 @@
 package stock;
 
-import java.util.AbstractCollection;
+import java.util.HashMap;
 
-public class Stock<T> {
+@SuppressWarnings("serial")
+public class Stock extends HashMap<Item, Integer>{
 	/**
 	 * A collection of items. Can be used for representing store inventory,
 	 * stock orders, sales logs, and truck cargo.
-	 */
-	
-	// Collect of stock of generic type T. 
-	private AbstractCollection<T> stock; // List of trucks 
-	
-	/** 
-	 * Constructor for the stock object. 
-	 * @param stock - the private field holding the collection of stock items. 
-	 */
-	
-	public Stock(AbstractCollection<T> stock) {
-		throw new UnsupportedOperationException("Not implemented yet");
-	}
+	 */	
+
 
 }
 
--- src/test/stock/StockTest.java
+++ src/test/stock/StockTest.java
@@ -6,9 +6,20 @@ import org.junit.Test;
 
 public class StockTest {
 
+	Item rice =  new Item("rice", 2, 3, 225, 300, null);
+	Stock shopStock = new Stock();
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addItemTest() {
+		shopStock.put(rice, 100);
+		
+		assertEquals(shopStock.size(), 1);
+	}
+	
+	@Test
+	public void itemQuantityTest() {
+		assertEquals(shopStock.get(rice), Double.valueOf(100));
 	}
 
 }
