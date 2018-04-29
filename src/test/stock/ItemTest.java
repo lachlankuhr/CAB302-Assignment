d2f144908ebc5ec8633e0e572a6c4cd67963178d
Atrey Gajjar <gajjar_atrey98@hotmail.com>
2018-04-29 12:56:59 +1000

    Initial getter tests for the Item class

--- src/test/stock/ItemTest.java
+++ src/test/stock/ItemTest.java
@@ -7,9 +7,47 @@ import org.junit.Test;
 
 public class ItemTest {
 
+	Item item;
+	
+	//Initialising an item for testing
+	@Before
+	public void initialiseItems() {
+		item = new Item("rice", 2, 3, 225, 300, null);		
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getNameTest() {
+		assertEquals(item.getName(), "rice");
+	}
+	
+	@Test
+	public void getManufacturingCostTest() {
+		assertEquals(item.getManufacturingCost(), 2);
+	}
+	
+	@Test
+	public void getSellPriceTest() {
+		assertEquals(item.getSellPrice(), 3);
+	}
+	
+	@Test
+	public void getReorderPointTest() {
+		assertEquals(item.getReorderPoint(), 225);
+	}
+	
+	@Test
+	public void getReorderAmountTest() {
+		assertEquals(item.getReorderAmount(), 300);
+	}
+	
+	@Test
+	public void getTemperatureTest() {
+		//Testing dry good
+		assertNull(item.getTemperature());
+		
+		//Test frozen good
+		Item frozenItem = new Item("ice cream", 8, 14, 175, 250, (double) -20);
+		assertEquals(frozenItem.getTemperature(), -20);
 	}
 
 }
