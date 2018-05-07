6ec25e3e44ea42986772eba41f6f8d0b560c8fcd
Atrey Gajjar <gajjar_atrey98@hotmail.com>
2018-05-07 15:02:00 +1000

    Added initial documentation to GUI

--- src/main/gui/GUI.java
+++ src/main/gui/GUI.java
@@ -29,11 +29,14 @@ import stock.Store;
 public class GUI extends JFrame implements ActionListener, Runnable {
 
 	/**
-	 * 
+	 * Variable declarations:
+	 * - Constants for the default size of the GUI, and default file paths
+	 * - Object creation for components of the GUI (including panels, buttons, label, etc)
 	 */
 	private static final long serialVersionUID = -3175409517730839439L;
 	private static final int WIDTH = 800;
 	private static final int HEIGHT = 600;
+	private final String DEFAULT_PROPERTIES_PATH = "\\files\\item_properties.csv";
 	
 	private JPanel buttonPnl = new JPanel();
 	private JPanel storeDataPnl = new JPanel();
@@ -49,29 +52,31 @@ public class GUI extends JFrame implements ActionListener, Runnable {
 	private JFileChooser fileChooser = new JFileChooser();
 	private JOptionPane optionPane = new JOptionPane();
 	
-	private final String DEFAULT_PROPERTIES_PATH = "\\files\\item_properties.csv";
 	
 	public static void main(String[] args) {
-		
-		Store store = Store.generateStoreInstance();
-		
+		//Thread safe creation of the GUI window
 		SwingUtilities.invokeLater(new GUI());
 	}
 	
 	/**
-	 * Entry point for GUI creation.
+	 * Entry point for GUI creation. Runs as soon as the program starts.
 	 */
 	@Override
 	public void run() {
 		createGUI();
 	}
 
+	/**
+	 * Event listening function that handles all button click events.
+	 * Opens a file selector on setting item properties, updating sales logs, and importing manifests.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object button = e.getSource();
 		if(button == itemPropBtn || button == salesLogBtn || button == importBtn) {
 			String filePath = fileSelectingAction();
 			
+			//If the user has selected a file, execute the relevant action associated with the button.
 			if(filePath != null) {
 				if(button == itemPropBtn) {
 					Stock.loadInItemProperties(filePath);
@@ -79,15 +84,20 @@ public class GUI extends JFrame implements ActionListener, Runnable {
 					Store.generateStoreInstance().loadSalesLog(filePath);
 				}else if(button == importBtn) {
 					Manifest manifest = new Manifest(filePath);
-					//Store.generateStoreInstance().importManifest(manifest);
+					Store.generateStoreInstance().importManifest(manifest);
 				}
 				
+				//Update the GUI table display to reflect these changes
 				((AbstractTableModel) stockDataTbl.getModel()).fireTableDataChanged();
 			}
 		}
 		
 	}
 	
+	/**
+	 * Helper method for opening a file selecting dialog and selecting a file.
+	 * @return The absolute file path of the selected file or {@code null} if not selected
+	 */
 	private String fileSelectingAction() {
 		int fileChooserReturn = fileChooser.showOpenDialog(this);
 		File file = null;
@@ -98,23 +108,33 @@ public class GUI extends JFrame implements ActionListener, Runnable {
 		return (file != null ? file.getAbsolutePath() : null);
 	}
 	
+	/**
+	 * Creates the GUI. Sets up the initial JFrame, and all panels and components of the GUI.
+	 * Also loads up the dialog box to select initial item properties to create the Store.
+	 */
 	private void createGUI() {
+		//Setting up the JFrame
 		setSize(WIDTH, HEIGHT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setDefaultLookAndFeelDecorated(true);
 		setLayout(new BorderLayout());
 		setTitle(Store.generateStoreInstance().getName() + " Inventory Management Application");
 
-
+		//Creating buttons, label for store capital, and table for store inventory.
 		createButtons();
 		displayStoreCapital();
 		displayStoreStock();
 		addPanels();
-		
 		this.setVisible(true);
+		
+		//Dialog box to select initial item properties.
 		initialItemPropertiesSelection();
 	}
 	
+	/**
+	 * Displays a dialog box prompting the user to select a file representing initial item properties.
+	 * Initial item properties is set to the file selected by the user, or otherwise a default included within the project.
+	 */
 	private void initialItemPropertiesSelection() {
 		String title = "Select item properties";
 		String message = "Select item properties. 'Cancel' uses default.";
@@ -133,6 +153,10 @@ public class GUI extends JFrame implements ActionListener, Runnable {
 		((AbstractTableModel) stockDataTbl.getModel()).fireTableDataChanged();
 	}
 	
+	/**
+	 * Sets up the button objects to have event listeners attached to the GUI JFrame, and added to the button panel.
+	 * Buttons are laid out to spread evenly across the width of the window.
+	 */
 	private void createButtons() {
 		buttonPnl.setLayout(new GridBagLayout());
 		
@@ -149,6 +173,9 @@ public class GUI extends JFrame implements ActionListener, Runnable {
 		buttonPnl.add(salesLogBtn, constraints);
 	}
 	
+	/**
+	 * Displays the store capital in a currency format on the GUI.	
+	 */
 	private void displayStoreCapital() {
 		storeDataPnl.setLayout(new BorderLayout());
 		storeCapitalLbl.setText("Store Capital: " + Store.generateStoreInstance().getFormattedCapital());
@@ -157,6 +184,10 @@ public class GUI extends JFrame implements ActionListener, Runnable {
 		storeDataPnl.add(storeCapitalLbl, BorderLayout.NORTH);
 	}
 	
+	/**
+	 * Displays a JTable representing the stock object of the store.
+	 * Places this within a scroll pane, to accommodate different lists of items.
+	 */
 	private void displayStoreStock() {
 		Stock inventory = Store.generateStoreInstance().getStock();
 		stockDataTbl.setModel(new StockTableModel(inventory));
@@ -165,6 +196,9 @@ public class GUI extends JFrame implements ActionListener, Runnable {
 		storeDataPnl.add(scrollPane);
 	}
 	
+	/**
+	 * Adds the button panel, and store data panel to the GUI.
+	 */
 	private void addPanels() {
 		getContentPane().add(buttonPnl, BorderLayout.NORTH);
 		getContentPane().add(storeDataPnl, BorderLayout.CENTER);
