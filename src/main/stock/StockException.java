7e2854baad276eb60ed0dad28f2bf914de1ead3f
Lachlan Kuhr <lachlankuhr@gmail.com>
2018-05-26 15:01:58 +1000

    Removed StockException constructor with no message

--- src/main/stock/StockException.java
+++ src/main/stock/StockException.java
@@ -2,15 +2,6 @@ package stock;
 
 @SuppressWarnings("serial")
 public class StockException extends Exception {
-	
-	/**
-	 * Constructs a StockException with null as its error message string.
-	 * @author Lachlan Kuhr
-	 */
-	public StockException() {
-		super();
-	}
-	
 	/**
 	 * Constructs a StockException with a particular error message string.
 	 * 
